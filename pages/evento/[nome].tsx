bb095bec2f3b394072556db24f217b0274b08771
marcelss10 <ambrosiomarcel@gmail.com>
2025-07-09 13:32:31 -0300

    💡 Ajustes de responsividade para mobile

--- pages/evento/[nome].tsx
+++ pages/evento/[nome].tsx
@@ -37,7 +37,8 @@ export default function Evento() {
         {fotos.length === 0 ? (
           <p className="text-lg text-gray-300">Nenhuma foto encontrada para este evento.</p>
         ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+
             {fotos.map((src, i) => (
               <PhotoCard key={i} src={src} />
             ))}
